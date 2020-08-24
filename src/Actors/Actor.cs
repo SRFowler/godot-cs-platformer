e32d370547f248e6318f57fb8ca4a9e8c4ec104f
Any Key <stephan.r.fowler@gmail.com>
2020-08-23 20:47:20 -0400

    Added player jump functionality
    Made Player class more pruely functional

--- src/Actors/Actor.cs
+++ src/Actors/Actor.cs
@@ -3,14 +3,10 @@ using Godot;
 public class Actor : KinematicBody2D
 {
     [Export]
-    private int gravity = 1000;
+    public int gravity = 1000;
     [Export]
-    public Vector2 speed = new Vector2(300, 1000);
-
+    public Vector2 speed = new Vector2(800, 800);
     public Vector2 velocity = new Vector2(0, 0);
     
-    public override void _PhysicsProcess(float delta) {
-        velocity.y += gravity * delta;
-        velocity = MoveAndSlide(velocity);
-    }
+
 }
--- src/Actors/Player.cs
+++ src/Actors/Player.cs
@@ -2,13 +2,39 @@ using Godot;
 
 public class Player : Actor
 {
-    private Vector2 direction = new Vector2(0, 1);
-    public override void _PhysicsProcess(float delta)
+ 
+     public override void _PhysicsProcess(float delta)
     {
-        direction.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-        velocity = direction * speed;
-        
+        Vector2 direction = GetDirection();
+
+        velocity = CalculateMoveVelocity(velocity, direction, speed);
+        velocity = MoveAndSlide(velocity, Vector2.Up);
+
         // Any time you override a method and want to call the parent method use this
-        base._PhysicsProcess(delta);
+        // base._PhysicsProcess(delta);
     }
+
+     private Vector2 GetDirection()
+     {
+          /// Returns a new Vector2 with both properties as floats from -1 to 1
+          return new Vector2(
+               Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
+               Input.IsActionJustPressed("jump") & IsOnFloor() ? -1 : 1
+          );
+     }
+
+     private Vector2 CalculateMoveVelocity(
+          Vector2 linearVelocity, 
+          Vector2 direction,
+          Vector2 speed)
+     {
+          Vector2 newVelocity = velocity;
+          newVelocity.x = speed.x * direction.x;
+          newVelocity.y += gravity * GetPhysicsProcessDeltaTime();
+          if (direction.y == -1.0)
+          {
+               newVelocity.y = speed.y * direction.y;
+          }
+          return newVelocity;
+     }
 }
